2748560f4ac0b09f9f8cf38316c7443fdb00f566
erra-ch <73989943+erra-ch@users.noreply.github.com>
2024-10-04 19:30:53 +0200

    almost

--- flink-app/cep/src/main/java/dat300/DataStreamJob.java
+++ flink-app/cep/src/main/java/dat300/DataStreamJob.java
@@ -1,32 +1,26 @@
 package dat300;
 
-import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
+import org.apache.flink.api.common.time.Time;
 import org.apache.flink.cep.CEP;
+import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.pattern.Pattern;
+import org.apache.flink.cep.pattern.conditions.IterativeCondition;
 import org.apache.flink.connector.file.sink.FileSink;
-import org.apache.flink.connector.file.src.FileSource;
-import org.apache.flink.connector.file.src.reader.TextLineInputFormat;
-import org.apache.flink.core.fs.FileSystem;
 import org.apache.flink.core.fs.Path;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
-import org.apache.flink.streaming.api.functions.sink.SinkFunction;
-import org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink;
-import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
+
+import java.util.List;
+import java.util.Map;
 
 public class DataStreamJob {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(4);
-        // Load a file
-//        FileSource<String> source =
-//            FileSource.forRecordStreamFormat(new TextLineInputFormat(), new Path("athena-sshd-processed.log"))
-//            .build();
 
 
         DataStream<EntryWithTimeStamp> stream = env.addSource(new DataIngestionSource(
@@ -36,36 +30,40 @@ public class DataStreamJob {
                 1000 * 5)
         );
 
-//        DataIngestionSource source = new DataIngestionSource(
-//                "athena-sshd-processed.log",
-//                1000,
-//                10,
-//                1000
-//        );
-//
-//        // Add FileSource to our execution environment
-//        DataStream<String> stream = env.fromSource(
-//            source,
-//            WatermarkStrategy.noWatermarks(),
-//            "FileSource"
-//        );
+        Pattern<EntryWithTimeStamp, ?> pattern = Pattern.<EntryWithTimeStamp>begin("firstInvalid")
+                .where(new IterativeCondition<EntryWithTimeStamp>() {
+                    @Override
+                    public boolean filter(EntryWithTimeStamp currentEvent, Context<EntryWithTimeStamp> ctx) throws Exception {
+                        System.out.println("Checking event");
+                        if (!currentEvent.getLogLine().contains("Invalid user")) {
+                            System.out.println("False");
+                            return false;
+                        }
+                        System.out.println("True");
 
+                        int attemptCount = 1;
+                        for (EntryWithTimeStamp previousEvent : ctx.getEventsForPattern("firstInvalid")) {
+                            if (currentEvent.getLogLine().split(" ")[8].equals(previousEvent.getLogLine().split(" ")[8])) {
+                                attemptCount++;
+                            }
+                        }
+                        return attemptCount > 1;
+                    }
+                })
+                .within(Time.minutes(1).toDuration());
 
-//DataStream<EntryWithTimeStamp> testStream = env.addSource(new LogFileStreamSimulator("./athena-sshd-processed.log")).name("foo");
+        PatternStream<EntryWithTimeStamp> patternStream = CEP.pattern(stream, pattern);
 
-//        DataStream<EntryWithTimeStamp> logLinesWithPreStamp = stream
-//                .map(new MapFunction<EntryWithTimeStamp, EntryWithTimeStamp>() {
-//                    @Override
-//                    int foo = 1;
-//                    public EntryWithTimeStamp map(EntryWithTimeStamp logLine) throws Exception {
-//                        long preTimeStamp = System.nanoTime();
-//                        EntryWithTimeStamp entry = new EntryWithTimeStamp(logLine,preTimeStamp);
-//                        foo += 1;
-//                        return entry;
-//                    }
-//                });
+        DataStream<EntryWithTimeStamp> patternMatches = patternStream.select(
+            new PatternSelectFunction<EntryWithTimeStamp, EntryWithTimeStamp>() {
+                @Override
+                public EntryWithTimeStamp select(Map<String, List<EntryWithTimeStamp>> pattern) throws Exception {
+                   return pattern.get("firstInvalid").get(0);
+                }
+            }
+        );
 
-        DataStream<EntryWithTimeStamp> exitStamp = stream
+        DataStream<EntryWithTimeStamp> exitStamp = patternMatches
                 .map(new MapFunction<EntryWithTimeStamp, EntryWithTimeStamp>() {
                     @Override
                     public EntryWithTimeStamp map(EntryWithTimeStamp entry) throws Exception {
@@ -84,10 +82,6 @@ public class DataStreamJob {
 
         exitStamp.sinkTo(outSink).setParallelism(1);
 
-        //DataStream<String> input = env.readTextFile("./outSink");
-        //input.writeAsText("./outSink/out.csv", FileSystem.WriteMode.OVERWRITE);
-
-        // Execute program, beginning computation.
         env.execute("DataStreamJob");
 
     }
