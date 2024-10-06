948cc9e21c48a35f046d819d418c89c870ebac62
erra-ch <73989943+erra-ch@users.noreply.github.com>
2024-10-06 13:48:07 +0200

    Repeated attempts from single IP Pattern
    Error 404, Reproducability not found...

--- flink-app/cep/src/main/java/dat300/DataStreamJob.java
+++ flink-app/cep/src/main/java/dat300/DataStreamJob.java
@@ -1,5 +1,6 @@
 package dat300;
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
 import org.apache.flink.api.common.time.Time;
@@ -14,43 +15,40 @@ import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.OnCheckpointRollingPolicy;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
 public class DataStreamJob {
     public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(4);
-
+        env.setParallelism(1);
 
         DataStream<EntryWithTimeStamp> stream = env.addSource(new DataIngestionSource(
                 "athena-sshd-processed.log",
                 1000,
                 10,
                 1000 * 5)
-        );
+        ).assignTimestampsAndWatermarks(WatermarkStrategy.<EntryWithTimeStamp>forBoundedOutOfOrderness(Duration.ofSeconds(10))
+                .withTimestampAssigner((entry, timestamp) -> entry.getPreTimeStamp()));
 
-        Pattern<EntryWithTimeStamp, ?> pattern = Pattern.<EntryWithTimeStamp>begin("firstInvalid")
+        Pattern<EntryWithTimeStamp, ?> pattern = Pattern.<EntryWithTimeStamp>begin("InvalidUser")
                 .where(new IterativeCondition<EntryWithTimeStamp>() {
                     @Override
                     public boolean filter(EntryWithTimeStamp currentEvent, Context<EntryWithTimeStamp> ctx) throws Exception {
-                        System.out.println("Checking event");
-                        if (!currentEvent.getLogLine().contains("Invalid user")) {
-                            System.out.println("False");
-                            return false;
-                        }
-                        System.out.println("True");
-
-                        int attemptCount = 1;
-                        for (EntryWithTimeStamp previousEvent : ctx.getEventsForPattern("firstInvalid")) {
+                        return currentEvent.getLogLine().contains("Invalid user");
+                    }
+                }).next("RepeatedIP").where(new IterativeCondition<EntryWithTimeStamp>() {
+                    @Override
+                    public boolean filter(EntryWithTimeStamp currentEvent, Context<EntryWithTimeStamp> ctx) throws Exception {
+                        for (EntryWithTimeStamp previousEvent : ctx.getEventsForPattern("InvalidUser")) {
                             if (currentEvent.getLogLine().split(" ")[8].equals(previousEvent.getLogLine().split(" ")[8])) {
-                                attemptCount++;
+                                return true;
                             }
                         }
-                        return attemptCount > 1;
+                        return false;
                     }
-                })
-                .within(Time.minutes(1).toDuration());
+                }).within(Time.minutes(10).toDuration());
 
         PatternStream<EntryWithTimeStamp> patternStream = CEP.pattern(stream, pattern);
 
@@ -58,7 +56,7 @@ public class DataStreamJob {
             new PatternSelectFunction<EntryWithTimeStamp, EntryWithTimeStamp>() {
                 @Override
                 public EntryWithTimeStamp select(Map<String, List<EntryWithTimeStamp>> pattern) throws Exception {
-                   return pattern.get("firstInvalid").get(0);
+                   return pattern.get("RepeatedIP").get(0);
                 }
             }
         );
